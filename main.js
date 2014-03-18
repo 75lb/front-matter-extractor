94093ff41cc2b1ceead42c48c15dc7bd74e41910
Lloyd Brookes <lloyd.brookes@gmail.com>
2014-03-18 15:50:13 +0100

    removed trim

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 var yaml = require("js-yaml");
 
 module.exports = function FrontMatterExtractor(input){
-    var matter = /^---$([\s\S]*)^---$/m;
+    var matter = /^---$([\s\S]*)^---\s/m;
     this.frontMatter = {};
     this.content = input;
     if (input && typeof input === "string"){
         var matches = input.match(matter);
         if (matches){
             this.frontMatter = yaml.safeLoad(matches[1]);
-            this.content = input.replace(matches[0], "").trim();
+            this.content = input.replace(matches[0], "");
         }
     } 
 }
